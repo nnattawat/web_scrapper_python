8d5c7252a043008e70160888cbcab2a2bab1df28
nnonsung <nnonsung@dius.com.au>
2020-06-18 19:18:38 +1000

    Add test case

--- lib/scrape_test.py
+++ lib/scrape_test.py
@@ -1,9 +1,21 @@
+import json
 import unittest
+from unittest.mock import patch
 from lib.scrape import get_popular_news
+from lib.test_data import HTML_TEXT
 
 class TestScrape(unittest.TestCase):
-  def test_get_news(self):
-    self.assertEqual(True, True)
+  @patch('lib.scrape.requests.get')
+  def test_get_news(self, mock_get):
+    mock_get.return_value.status_code = 200
+    mock_get.return_value.text = HTML_TEXT 
+
+    response = get_popular_news(900)
+    self.assertEqual(response, [{
+      'score': 973,
+      'title': 'Time to Upgrade Your Monitor',
+      'url': 'https://tonsky.me/blog/monitors/'
+    }])
 
 
 if __name__ == "__main__":
